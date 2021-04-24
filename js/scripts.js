2f83cbf9302fc9078e6047bcfd322f6e5feec185
TKimba <72319883+TKimba@users.noreply.github.com>
2021-04-24 16:29:18 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,43 @@
 
+$(document).ready(function () {
 
 
+    /* 
+     * Test Code
+     * Controls located in footer
+     */
+    var btn_hit = document.getElementById('test_hit');
 
-/*
- * Test Code
- * Controls located in footer
- */
 
-$("#test_hit").click(hit());
-$("#test_miss").click(miss());
+    $(btn_hit).click(hit());
+    $("#test_miss").click(miss());
+    
+    
+    function hit() {
+        console.log('hit!');
+    }
 
-function hit(){
-    console.log('hit!');
-}
+    function miss() {
+        console.log('miss!');
+    }
 
-function miss(){
-    console.log('miss!');
-}
+    function range() {
+        Math.floor((Math.random() * 5) + 1);
+    }
 
-function range(){
-    Math.floor((Math.random() * 5) + 1);
-}
-
-/*Pseudocode
- *
- * user presses Go!
- *
- * capture inputs
- *
- * calculate landing location
- *
- * if (landing == target)
- *      hit! Score increases
- *      relocate target
- * else
- *      miss! TP count decreases
- *
- */
+    /*Pseudocode
+     *
+     * user presses Go!
+     *
+     * capture inputs
+     *
+     * calculate landing location
+     *
+     * if (landing == target)
+     *      hit! Score increases
+     *      relocate target
+     * else
+     *      miss! TP count decreases
+     *
+     */
+});
