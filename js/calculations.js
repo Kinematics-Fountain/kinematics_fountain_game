89304139a5687e12e8717103d0a5829b4ad19cca
Kimberly Gahn <kimberly.gahn@gmail.com>
2021-04-25 13:00:08 +0900

    Update calculations.js

--- js/calculations.js
+++ js/calculations.js
@@ -1,8 +1,6 @@
 // Define calculation function
 function projectileCalculate(v1,theta,dy,ay)
 {
-  var dy = -1
-  var ay = -9.8
   var v1y = v1 * Math.sin(theta/180*Math.PI);
   return Math.round(v1 * Math.cos(theta/180*Math.PI) * (-v1y - Math.sqrt(v1y*v1y + 2*ay*dy))/ay);
 }
@@ -11,30 +9,27 @@ function projectileCalculate(v1,theta,dy,ay)
 function compareRange(gRange,cRange)
 {
     if (gRange == cRange) {
-        result = "True";
+        result = true;
     }
     else {
-        result = "False";
+        result = false;
     }
+    return result;
 }
+
 // Define function that results in "Go!" button click
 function doClick()
 {
-  var v1 = parseFloat($("#input_velocity").val());
-  var theta = parseFloat($("#input_angle").val());
-  var dy = -1;
-  var ay = -9.8;
-  var gRange = parseFloat($("#input_range").val()); // given range
-  var cRange = projectileCalculate(v1,theta,dy,ay); // calculated range
-  var result = compareRange(gRange,cRange);
-  var message = "The projectile will travel ";
-  message += result;
-  message += "m horizontally.<br/>";
-
-  //$("#output").html(message);
+    var v1 = parseFloat($("#input_velocity").val());
+    var theta = parseFloat($("#input_angle").val());
+    var dy = -1;
+    var ay = -9.8;
+    var gRange = parseFloat($("#input_range").val()); // given range
+    var cRange = projectileCalculate(v1,theta,dy,ay); // calculated range
+    var result = compareRange(gRange,cRange);
+    var message = "The input rang = the calculated range: ";
+    message += result;
 
 
-
-document.getElementById("response").innerHTML = message;
+    $("#output").html(message);
 }
-document.getElementById("btn_go").onclick = doClick;
