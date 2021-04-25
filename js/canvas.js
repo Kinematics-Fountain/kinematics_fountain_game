8b4601abd42575d9d9d8ab182df6cedcb0a491e9
TKimba <72319883+TKimba@users.noreply.github.com>
2021-04-24 21:05:08 -0700

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,36 @@
 /* 
  * HTML5 Canvas
  */
-$(function(){
+$(function () {
+
+    canvas = document.getElementById("gameCanvas");
+    //canvasContext = canvas.getContext('2d');
+    
+    //Images
+    var tp = new Image();
+    tp.src = '../imgs/tp.png';
+    
+    var statue = new Image();
+    statue.src = '../imgs/statue.png';
+    
+    var lily = new Image();
+    lily.src = '../imgs/lilypad.png';
+    
     
 
-function startGame() {
-    myGameArea.start();
-}
-
-var myGameArea = {
-    canvas : document.getElementById("gameCanvas"),
-    start : function() {
-        this.canvas.width = 700;
-        this.canvas.height = 700;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    function startGame() {
+        myGameArea.start();
     }
-};
+
+    var myGameArea = {
+        canvas: canvas,
+        start: function () {
+            this.canvas.width = 700;
+            this.canvas.height = 700;
+            this.context = this.canvas.getContext("2d");
+            document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        }
+    };
 });
 
 
